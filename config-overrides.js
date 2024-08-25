8501837d6bcd29f32c02a5d271d8ac4200ed61d2
bp117 <bindupriya117@gmail.com>
2024-08-26 01:20:18 +0530

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  webpack: function (config, env) {
-    // Disable file name hashing
-    config.output.filename = 'static/js/searchWidget.js';
-    config.output.chunkFilename = 'static/js/[name].chunk.js';
+  webpack: function (config) {
+    // Set up multiple entry points for TypeScript files
+    config.entry = {
+      main: path.resolve(__dirname, 'src/index.tsx'), // Main application entry point
+      widget: path.resolve(__dirname, 'src/widget/index.tsx'), // Widget entry point
+    };
 
-    // Ensure the output path is what you expect
-    config.output.path = path.resolve(__dirname, 'build');
+    config.output = {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'static/js/[name].bundle.js', // Generates separate bundles
+    };
 
     return config;
-  }
+  },
+  // Optionally, customize the Jest config for TypeScript
+  jest: function (config) {
+    config.globals = { 'ts-jest': { tsconfig: 'tsconfig.json' } };
+    config.moduleFileExtensions.push('ts', 'tsx');
+    return config;
+  },
 };
